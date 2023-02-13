19e52943ada193df958aefdb349d07fccd88656e
DESKTOP-KP43KU2\Gilbert Alexandre <gilbert.alexandre.eng@gmai.com>
2023-02-13 08:22:50 -0300

    fase15-finalizacao-texto-tcc

--- projetos/amu/app/src/main/java/br/ufpa/app/android/amu/v1/util/Constantes.java
+++ projetos/amu/app/src/main/java/br/ufpa/app/android/amu/v1/util/Constantes.java
@@ -48,9 +48,9 @@ public class Constantes {
 
     public static final String[] intervalos = new String[] {
             "6 horas", "8 horas", "12 horas", "24 horas",
-            "1 horas", "2 horas", "3 horas", "4 horas", "5 horas", "6 horas",
-            "7 horas", "8 horas", "9 horas", "10 horas", "11 horas", "12 horas",
-            "13 horas", "14 horas", "15 horas", "16 horas", "17 horas", "18 horas",
-            "19 horas", "20 horas", "21 horas", "22 horas", "23 horas", "24 horas"};
+            "1 horas", "2 horas", "3 horas", "4 horas", "5 horas",
+            "7 horas", "9 horas", "10 horas", "11 horas", "13 horas",
+            "14 horas", "15 horas", "16 horas", "17 horas", "18 horas",
+            "19 horas", "20 horas", "21 horas", "22 horas", "23 horas"};
 
 }
